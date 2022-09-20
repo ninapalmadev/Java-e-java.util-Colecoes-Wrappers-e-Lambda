0473ebc1065b54b296f7953ec190da911e7d532d
Nina Palma <ninapalmadev@gmail.com>
2022-09-20 12:20:24 -0300

    cria array com Object

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
@@ -1,6 +1,9 @@
 package br.com.bytebank.banco.test;
 
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
@@ -8,20 +11,23 @@ public class TesteArrayReferencias {
 		
 		//int[] idades = new int[5]
 		
-		ContaCorrente[] contas = new ContaCorrente[5];
+		Object[] referencias = new Object[5];
 	
 		ContaCorrente cc1 = new ContaCorrente(3131, 13);
+		referencias[0] = cc1;
 		
-		contas[0] = cc1;
+		ContaPoupanca cc2 = new ContaPoupanca(3131, 31);
+		referencias[1] = cc2;
 		
-		ContaCorrente cc2 = new ContaCorrente(3131, 31);
-		contas[1] = cc2;
+		Cliente cliente = new Cliente();
+		referencias[2] = cliente;
 		
 		//System.out.println(cc2.getNumero());
-		System.out.println(contas[1].getNumero());
-		System.out.println(contas[0].getNumero());
 		
-		ContaCorrente ref = contas[1];
+//		System.out.println(contas[1].getNumero());
+//		System.out.println(contas[0].getNumero());
+		
+		ContaPoupanca ref = (ContaPoupanca)referencias[1]; //type cast
 		System.out.println(ref.getNumero());
 	}
 
