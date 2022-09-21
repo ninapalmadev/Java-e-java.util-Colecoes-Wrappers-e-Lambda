62c0f7daa4c0a1b6da278cd18937389e9d8e1cfd
Nina Palma <ninapalmadev@gmail.com>
2022-09-21 12:54:20 -0300

    métodos da class Integer

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
@@ -1,10 +1,34 @@
 package br.com.bytebank.banco.test.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Teste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int idade = 28;// Integer
+		Integer idadeRef = Integer.valueOf(29); //autoboxing
+		System.out.println(idadeRef.doubleValue());
+		
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		
+		System.out.println(Integer.SIZE);
+		System.out.println(Integer.BYTES);
+		
+		
+		int valor = idadeRef.intValue(); //unboxing
+		String s = args[0]; //"10"
+		//Integer numeroDoTerminal = Integer.valueOf(s);
+		int numeroDoTerminal = Integer.parseInt(s);
+		System.out.println(numeroDoTerminal);
+		
+
+		
+		List<Integer> numeros = new ArrayList<Integer>();
+		numeros.add(29); //autoboxing
 
 	}
-
 }
